75c150d25d67ce2da728ba743566d5985d7cf71e
staimu@staimutanjungpinang.ac.id <staimu@mantang.yacanet.com>
2020-12-12 10:27:09 +0700

    backend untuk crud zoom api

---
+++ backend/app/Http/Controllers/Plugins/H2H/ZoomAPI/ZoomController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Http\Controllers\Plugins\H2H\ZoomAPI;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+use App\Models\Plugins\H2H\ZoomAPI\ZoomModel;
+
+use Ramsey\Uuid\Uuid;
+
+class ZoomController extends Controller {
+     /**
+     * daftar account zoom
+     */
+    public function index(Request $request)
+    {
+        $zoom=ZoomModel::all();
+
+        return Response()->json([
+                                    'status'=>1,
+                                    'pid'=>'fetchdata',
+                                    'zoom'=>$zoom,
+                                    'message'=>'Fetch data account zoom berhasil.'
+                                ],200);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->hasPermissionTo('PLUGINS-H2H-ZOOMAPI_STORE');
+        $rule=[
+            'email'=>'required|email|unique:plugins_h2h_zoom',
+            'api_key'=>'required|string|unique:plugins_h2h_zoom',
+            'api_secret'=>'required|string|unique:plugins_h2h_zoom',
+            'im_token'=>'required|string|unique:plugins_h2h_zoom',
+        ];
+
+        $this->validate($request, $rule);
+
+        $zoom=ZoomModel::create([
+            'id'=>Uuid::uuid4()->toString(),
+            'email'=>$request->input('email'),
+            'api_key'=>$request->input('api_key'),
+            'api_secret'=>$request->input('api_secret'),
+            'im_token'=>$request->input('im_token'),
+        ]);
+
+        \App\Models\System\ActivityLog::log($request,[
+                                        'object' => $zoom,
+                                        'object_id'=>$zoom->id,
+                                        'user_id' => $this->getUserid(),
+                                        'message' => 'Menambah account zoom baru berhasil'
+                                    ]);
+
+        return Response()->json([
+                                    'status'=>1,
+                                    'pid'=>'store',
+                                    'zoom'=>$zoom,
+                                    'message'=>'Data account zoom berhasil disimpan.'
+                                ],200);
+
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->hasPermissionTo('PLUGINS-H2H-ZOOMAPI_UPDATE');
+
+        $zoom = ZoomModel::find($id);
+        if (is_null($zoom))
+        {
+            return Response()->json([
+                                    'status'=>0,
+                                    'pid'=>'update',
+                                    'message'=>["account zoom ($id) gagal diupdate"]
+                                ],422);
+        }
+        else
+        {
+
+            $this->validate($request, [
+
+                                        'email'=>[
+                                                        'required',
+                                                        'string',
+                                                        Rule::unique('plugins_h2h_zoom')->ignore($zoom->email,'email')
+                                                    ],
+                                        'api_key'=>[
+                                                        'required',
+                                                        'string',
+                                                        Rule::unique('plugins_h2h_zoom')->ignore($zoom->api_key,'api_key')
+                                                    ],
+                                        'api_secret'=>[
+                                                        'required',
+                                                        'string',
+                                                        Rule::unique('plugins_h2h_zoom')->ignore($zoom->api_secret,'api_secret')
+                                                    ],
+                                        'im_token'=>[
+                                                        'required',
+                                                        'string',
+                                                        Rule::unique('plugins_h2h_zoom')->ignore($zoom->im_token,'im_token')
+                                                    ],
+
+
+
+                                    ]);
+
+            $zoom->email = $request->input('email');
+            $zoom->api_key = $request->input('api_key');
+            $zoom->api_secret = $request->input('api_secret');
+            $zoom->im_token = $request->input('im_token');
+
+            $zoom->save();
+
+            \App\Models\System\ActivityLog::log($request,[
+                                                        'object' => $zoom,
+                                                        'object_id'=>$zoom->id,
+                                                        'user_id' => $this->getUserid(),
+                                                        'message' => 'Mengubah data account zoom ('.$zoom->email.') berhasil'
+                                                    ]);
+
+            return Response()->json([
+                                    'status'=>1,
+                                    'pid'=>'update',
+                                    'zoom'=>$zoom,
+                                    'message'=>'Data account zoom '.$zoom->email.' berhasil diubah.'
+                                ],200);
+        }
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $id)
+    {
+        $this->hasPermissionTo('PLUGINS-H2H-ZOOMAPI_SHOW');
+
+        $zoom = ZoomModel::find($id);
+        if (is_null($zoom))
+        {
+            return Response()->json([
+                                    'status'=>0,
+                                    'pid'=>'update',
+                                    'message'=>["account zoom ($id) gagal diperoleh"]
+                                ],422);
+        }
+        else
+        {
+
+            return Response()->json([
+                                    'status'=>1,
+                                    'pid'=>'fetchdata',
+                                    'zoom'=>$zoom,
+                                    'message'=>'Data account zoom '.$zoom->email.' berhasil diperoleh.'
+                                ],200);
+        }
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request,$id)
+    {
+        $this->hasPermissionTo('PLUGINS-H2H-ZOOMAPI_DESTROY');
+
+        $zoom = ZoomModel::find($id);
+
+        if (is_null($zoom))
+        {
+            return Response()->json([
+                                    'status'=>0,
+                                    'pid'=>'destroy',
+                                    'message'=>["Kode account zoom ($id) gagal dihapus"]
+                                ],422);
+        }
+        else
+        {
+            \App\Models\System\ActivityLog::log($request,[
+                                                                'object' => $zoom,
+                                                                'object_id' => $zoom->id,
+                                                                'user_id' => $this->getUserid(),
+                                                                'message' => 'Menghapus account zoom ('.$id.') berhasil'
+                                                            ]);
+            $zoom->delete();
+            return Response()->json([
+                                        'status'=>1,
+                                        'pid'=>'destroy',
+                                        'message'=>"Account zoom dengan id ($id) berhasil dihapus"
+                                    ],200);
+        }
+
+    }
+}
--- frontend/src/views/pages/admin/system/Permissions.vue
+++ frontend/src/views/pages/admin/system/Permissions.vue
@@ -15,17 +15,17 @@
                 </v-breadcrumbs>
             </template>
             <template v-slot:desc>
-                <v-alert                                        
+                <v-alert
                     color="cyan"
-                    border="left"                    
+                    border="left"
                     colored-border
                     type="info"
                     >
                     Daftar aksi-aksi terhadap sebuah modul. Format penulisan permission, NAMAMODULE atau NAMA MODULE. Nama Permission tighly coupling dengan kode sumber.
                     </v-alert>
             </template>
-        </ModuleHeader>        
-        <v-container fluid>    
+        </ModuleHeader>
+        <v-container fluid>
             <v-row class="mb-4" no-gutters>
                 <v-col xs="12" sm="12" md="12">
                     <v-card>
@@ -67,36 +67,36 @@
                                 ></v-divider>
                                 <v-spacer></v-spacer>
                                 <v-dialog v-model="dialog" max-width="500px" persistent>
-                                    <template v-slot:activator="{ on }">                                        
+                                    <template v-slot:activator="{ on }">
                                         <v-btn color="primary" icon outlined small class="ma-2" v-on="on" :disabled="!CAN_ACCESS('PERMISSIONS_STORE')">
                                             <v-icon>mdi-plus</v-icon>
-                                        </v-btn>                                        
+                                        </v-btn>
                                     </template>
                                     <v-form ref="frmdata" v-model="form_valid" lazy-validation>
                                         <v-card>
                                             <v-card-title>
                                                 <span class="headline">{{ formTitle }}</span>
                                             </v-card-title>
-                                            <v-card-text>                                                
+                                            <v-card-text>
                                                 <v-container fluid>
                                                     <v-row>
                                                         <v-col cols="12" sm="12" md="12">
-                                                            <v-text-field 
-                                                                v-model="editedItem.name" 
+                                                            <v-text-field
+                                                                v-model="editedItem.name"
                                                                 label="NAMA PERMISSION"
                                                                 :rules="rule_permission_name">
                                                             </v-text-field>
-                                                        </v-col>                                            
+                                                        </v-col>
                                                     </v-row>
                                                 </v-container>
                                             </v-card-text>
                                             <v-card-actions>
                                                 <v-spacer></v-spacer>
                                                 <v-btn color="blue darken-1" text @click.stop="close">BATAL</v-btn>
-                                                <v-btn 
-                                                    color="blue darken-1" 
-                                                    text 
-                                                    @click.stop="save" 
+                                                <v-btn
+                                                    color="blue darken-1"
+                                                    text
+                                                    @click.stop="save"
                                                     :loading="btnLoading"
                                                     :disabled="!form_valid||btnLoading">
                                                         SIMPAN
@@ -163,64 +163,64 @@ export default {
     data: () => ({
         breadcrumbs:[],
         datatableLoading:false,
-        btnLoading:false,  
-        expanded:[],        
+        btnLoading:false,
+        expanded:[],
         daftar_permissions: [],
         //tables
-        headers: [                        
+        headers: [
             { text: 'NAMA PERMISSION', value: 'name' },
-            { text: 'GUARD', value: 'guard_name' },            
+            { text: 'GUARD', value: 'guard_name' },
             { text: 'AKSI', value: 'actions', sortable: false,width:100 },
         ],
-        search:'',        
+        search:'',
         //form
         form_valid:true,
         dialog: false,
         editedIndex: -1,
         editedItem: {
             id:0,
-            name: '',           
-            guard: '',           
-            created_at: '',           
-            updated_at: '',           
+            name: '',
+            guard: '',
+            created_at: '',
+            updated_at: '',
         },
         defaultItem: {
             id:0,
-            name: '',           
-            guard: 'api',           
-            created_at: '',           
-            updated_at: '',           
+            name: '',
+            guard: 'api',
+            created_at: '',
+            updated_at: '',
         },
-        //form rules        
+        //form rules
         rule_permission_name:[
-            value => !!value||"Mohon untuk di isi nama Permission !!!",  
-            value => /^[a-zA-Z\\-]+$/.test(value) || 'Nama Permission hanya boleh string',                
-        ], 
+            value => !!value||"Mohon untuk di isi nama Permission !!!",
+            value => /^[0-9\\a-zA-Z\\-]+$/.test(value) || 'Nama Permission hanya boleh angka,huruf,dan tanda -',                
+        ],
     }),
     methods: {
-        initialize () 
+        initialize ()
         {
             this.datatableLoading=true;
             this.$ajax.get('/system/setting/permissions',{
                 headers: {
                     Authorization:this.TOKEN
                 }
-            }).then(({data})=>{                
+            }).then(({data})=>{
                 this.daftar_permissions = data.permissions;
                 this.datatableLoading=false;
-            });          
-            
+            });
+
         },
         dataTableRowClicked(item)
         {
             if ( item === this.expanded[0])
             {
-                this.expanded=[];                
+                this.expanded=[];
             }
             else
             {
                 this.expanded=[item];
-            }               
+            }
         },
         editItem (item) {
             this.editedIndex = this.daftar_permissions.indexOf(item)
@@ -230,7 +230,7 @@ export default {
         close () {
             this.btnLoading=false;
             this.dialog = false;
-            this.$refs.frmdata.reset(); 
+            this.$refs.frmdata.reset();
             setTimeout(() => {
                 this.editedItem = Object.assign({}, this.defaultItem)
                 this.editedIndex = -1
@@ -240,7 +240,7 @@ export default {
         save () {
             if (this.$refs.frmdata.validate())
             {
-                if (!(this.editedIndex > -1)) 
+                if (!(this.editedIndex > -1))
                 {
                     this.btnLoading=true;
                     this.$ajax.post('/system/setting/permissions/store',
@@ -252,7 +252,7 @@ export default {
                                 Authorization:this.TOKEN
                             }
                         }
-                    ).then(()=>{   
+                    ).then(()=>{
                         this.initialize();
                         this.close();
                     }).catch(()=>{
@@ -261,7 +261,7 @@ export default {
                 }
             }
         },
-        deleteItem (item) {   
+        deleteItem (item) {
             this.$root.$confirm.open('Delete', 'Apakah Anda ingin menghapus permission '+item.name+' ?', { color: 'red' }).then((confirm) => {
                 if (confirm)
                 {
@@ -275,7 +275,7 @@ export default {
                             Authorization:this.TOKEN
                         }
                     }
-                    ).then(()=>{   
+                    ).then(()=>{
                         const index = this.daftar_permissions.indexOf(item);
                         this.daftar_permissions.splice(index, 1);
                         this.btnLoading=false;
@@ -283,25 +283,25 @@ export default {
                         this.btnLoading=false;
                     });
                 }
-            });      
+            });
         },
     },
     computed:{
         formTitle () {
             return this.editedIndex === -1 ? 'TAMBAH PERMISSION' : 'EDIT PERMISSION'
         },
-        ...mapGetters('auth',{            
-            ACCESS_TOKEN:'AccessToken',          
-            TOKEN:'Token',          
-            CAN_ACCESS:'can',         
-            ATTRIBUTE_USER:'AttributeUser',          
+        ...mapGetters('auth',{
+            ACCESS_TOKEN:'AccessToken',
+            TOKEN:'Token',
+            CAN_ACCESS:'can',
+            ATTRIBUTE_USER:'AttributeUser',
         }),
     },
     watch: {
         dialog (val) {
             val || this.close()
         },
-    },   
+    },
     components:{
 		SystemUserLayout,
 		ModuleHeader,
