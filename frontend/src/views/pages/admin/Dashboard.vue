629fd964e3548441bd7185fb762323b97f54cb7e
Aggry Saputra <aggrysaputra@gmail.com>
2020-12-16 22:06:04 +0700

    Rename Css portalekampus.css jadi campus50.css

--- frontend/src/views/pages/admin/Dashboard.vue
+++ frontend/src/views/pages/admin/Dashboard.vue
@@ -135,57 +135,20 @@
                 <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>            
                 <v-col xs="12" sm="4" md="3" v-if="$store.getters['auth/can']('SYSTEM-USERS-GROUP')">
                     <template>
-  <v-card
-    class="mx-auto"
-    color="#26c6da"
-    dark
-    max-width="400"
-  >
-    <v-card-title>
-      <v-icon
-        large
-        left
+  <v-col
+        cols="12"
+        sm="6"
+        lg="3"
       >
-        mdi-twitter
-      </v-icon>
-      <span class="title font-weight-light">Twitter</span>
-    </v-card-title>
-
-    <v-card-text class="headline font-weight-bold">
-      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
-    </v-card-text>
-
-    <v-card-actions>
-      <v-list-item class="grow">
-        <v-list-item-avatar color="grey darken-3">
-          <v-img
-            class="elevation-6"
-            alt=""
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Evan You</v-list-item-title>
-        </v-list-item-content>
-
-        <v-row
-          align="center"
-          justify="end"
-        >
-          <v-icon class="mr-1">
-            mdi-heart
-          </v-icon>
-          <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
-          <v-icon class="mr-1">
-            mdi-share-variant
-          </v-icon>
-          <span class="subheading">45</span>
-        </v-row>
-      </v-list-item>
-    </v-card-actions>
-  </v-card>
+        <base-material-stats-card
+          color="info"
+          icon="mdi-twitter"
+          title="Followers"
+          value="+245"
+          sub-icon="mdi-clock"
+          sub-text="Just Updated"
+        />
+      </v-col>
 </template>
                 </v-col>
                 <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>            
