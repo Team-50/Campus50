f52fbbd1b0bfad269a36762dfd34b2a1c7a1f684
Aggry Saputra <aggrysaputra@gmail.com>
2020-12-26 14:29:51 +0700

    Tambah logo Campus 50

--- frontend/src/views/layouts/FrontLayout.vue
+++ frontend/src/views/layouts/FrontLayout.vue
@@ -2,7 +2,12 @@
     <div>
         <v-app-bar app>
             <v-toolbar-title>
-                <span class="hidden-sm-and-down">{{namaPTAlias}}</span>
+                <span class="hidden-sm-and-down">
+                    <v-img
+                        max-width="80"
+                        :src="$api.url+'/storage/images/campus_50_logo.png'"
+                        ></v-img>
+                </span>
             </v-toolbar-title>
             <v-spacer />
             <v-btn to="/" class="mr-2" color="primary" text large>
@@ -17,7 +22,29 @@
         </v-app-bar>
         <v-main>
             <slot/>
-        </v-main>        
+        </v-main>     
+
+        <v-footer
+            dark
+            padless
+        >
+            <v-card
+            class="flex"
+            flat
+            tile
+            >
+            <v-card-title class="teal">
+                <strong class="subheading">Get connected with us on social networks!</strong>
+
+                <v-spacer></v-spacer>
+            </v-card-title>
+
+            <v-card-text class="py-2 white--text text-center">
+                {{ new Date().getFullYear() }} — <strong>{{namaPTAlias}}</strong>
+            </v-card-text>
+            </v-card>
+        </v-footer>
+
     </div>
 </template>
 <script>
