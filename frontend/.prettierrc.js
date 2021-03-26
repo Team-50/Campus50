f6b8c85f7b1f52bb9028a64fb256107c36aa4a04
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2021-03-26 13:58:54 +0700

    dashboard campus 50, redirect

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  vueIndentScriptAndStyle: true,
+  proseWrap:"never",
+  printWidth: 80,
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  jsxBracketSameLine: true,   
+  indent: [2,"tab"],
+};
