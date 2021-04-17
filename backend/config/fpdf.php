d8979a713345af41553e379dceb3b68423803c0a
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2021-04-18 00:14:46 +0700

    trying to fix local en

---
+++ backend/config/fpdf.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'orientation'       => 'P',
+    'unit'              => 'mm',
+    'size'              => 'A4',
+    'useVaporHeaders'  => env('FPDF_VAPOR_HEADERS', false),
+];
