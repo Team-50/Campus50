85562bb866bf0df6cede9c1dc8b05390ba2b482b
Aggry Saputra <aggrysaputra@gmail.com>
2021-04-27 01:10:32 +0700

    prettier admin layout~

--- frontend/src/views/layouts/AdminLayout.vue
+++ frontend/src/views/layouts/AdminLayout.vue
@@ -1,130 +1,130 @@
 <template>
-    <div>
-        <v-system-bar app color="#a4b0be" class="white--text">
-            <strong>Hak Akses Sebagai :</strong> {{ROLE}}
+		<div>
+				<v-system-bar app color="#a4b0be" class="white--text">
+						<strong>Hak Akses Sebagai :</strong> {{ROLE}}
 		</v-system-bar>
-        <v-app-bar app class="white" elevation="0">            
-            <v-toolbar-title class="headline clickable" @click.stop="$router.push('/dashboard/'+$store.getters['auth/AccessToken']).catch(err => {})">
+				<v-app-bar app class="white" elevation="0">            
+						<v-toolbar-title class="headline clickable" @click.stop="$router.push('/dashboard/'+$store.getters['auth/AccessToken']).catch(err => {})">
 				<span class="hidden-sm-and-down">{{APP_NAME}}</span>
 			</v-toolbar-title>
-            <v-spacer></v-spacer>            
-            <v-divider
-                class="mx-4"
-                inset
-                vertical
-            ></v-divider>
-            <v-menu
+						<v-spacer></v-spacer>            
+						<v-divider
+								class="mx-4"
+								inset
+								vertical
+						></v-divider>
+						<v-menu
 
-                :close-on-content-click="true"
-                origin="center center"
-                transition="scale-transition"
-                :offset-y="true"
-                bottom 
-                left>
-                <template v-slot:activator="{on}">
-                    <v-avatar size="30">
-                        <v-img :src="photoUser" v-on="on" />
-                    </v-avatar>                    
-                </template>
-                <v-list>
-                    <v-list-item>
-                        <v-list-item-avatar>
-                            <v-img :src="photoUser"></v-img>
-                        </v-list-item-avatar>
-                        <v-list-item-content>					
-                            <v-list-item-title class="title">
-                                {{ATTRIBUTE_USER('username')}}
-                            </v-list-item-title>
-                            <v-list-item-subtitle>                                
-                                [{{DEFAULT_ROLE}}]
-                            </v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>                    
-                    <v-divider/>
-                    <v-list-item to="/system-users/profil">
-                        <v-list-item-icon class="mr-2">
+								:close-on-content-click="true"
+								origin="center center"
+								transition="scale-transition"
+								:offset-y="true"
+								bottom 
+								left>
+								<template v-slot:activator="{on}">
+										<v-avatar size="30">
+												<v-img :src="photoUser" v-on="on" />
+										</v-avatar>                    
+								</template>
+								<v-list>
+										<v-list-item>
+												<v-list-item-avatar>
+														<v-img :src="photoUser"></v-img>
+												</v-list-item-avatar>
+												<v-list-item-content>					
+														<v-list-item-title class="title">
+																{{ATTRIBUTE_USER('username')}}
+														</v-list-item-title>
+														<v-list-item-subtitle>                                
+																[{{DEFAULT_ROLE}}]
+														</v-list-item-subtitle>
+												</v-list-item-content>
+										</v-list-item>                    
+										<v-divider/>
+										<v-list-item to="/system-users/profil">
+												<v-list-item-icon class="mr-2">
 							<v-icon>mdi-account</v-icon>
 						</v-list-item-icon>
-                        <v-list-item-title>Profil</v-list-item-title>
-                    </v-list-item>
-                    <v-divider/>
-                    <v-list-item @click.prevent="logout">
-                        <v-list-item-icon class="mr-2">
+												<v-list-item-title>Profil</v-list-item-title>
+										</v-list-item>
+										<v-divider/>
+										<v-list-item @click.prevent="logout">
+												<v-list-item-icon class="mr-2">
 							<v-icon>mdi-power</v-icon>
 						</v-list-item-icon>
-                        <v-list-item-title>Logout</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>			
-        </v-app-bar>                    
-        <v-main class="mx-4 mb-4 grey lighten-3">			
+												<v-list-item-title>Logout</v-list-item-title>
+										</v-list-item>
+								</v-list>
+						</v-menu>			
+				</v-app-bar>                    
+				<v-main class="mx-4 mb-4 grey lighten-3">			
 			<slot />
 		</v-main>
-    </div>    
+		</div>    
 </template>
 <script>
 import {mapGetters} from 'vuex';
 export default {
-    name:'AdminLayout',  
-    props:{
-        showrightsidebar:{
-            type:Boolean,
-            default:true
-        },
-        temporaryleftsidebar:{
-            type:Boolean,
-            default:false
-        },
-    },      
-    data:()=>({
-        loginTime:0,
-        drawer:null,
-        drawerRight:null,   
-    }),       
-    methods: {        
-        logout ()
-        {
-            this.loginTime=0;
-            this.$ajax.post('/auth/logout',
-                {},
-                {
-                    headers:{
-                        'Authorization': this.TOKEN,
-                    }
-                }
-            ).then(()=> {     
-                this.$store.dispatch('auth/logout');	
-                this.$store.dispatch('uifront/reinit');	
-                this.$store.dispatch('uiadmin/reinit');	
-                this.$router.push('/');
-            })
-            .catch(() => {
-                this.$store.dispatch('auth/logout');	
-                this.$store.dispatch('uifront/reinit');	
-                this.$store.dispatch('uiadmin/reinit');	
-                this.$router.push('/');
-            });
-        },
-        isBentukPT (bentuk_pt)
-        {
-            return this.$store.getters['uifront/getBentukPT']==bentuk_pt?true:false;
-        }
+		name:'AdminLayout',  
+		props:{
+				showrightsidebar:{
+						type:Boolean,
+						default:true
+				},
+				temporaryleftsidebar:{
+						type:Boolean,
+						default:false
+				},
+		},      
+		data:()=>({
+				loginTime:0,
+				drawer:null,
+				drawerRight:null,   
+		}),       
+		methods: {        
+				logout ()
+				{
+						this.loginTime=0;
+						this.$ajax.post('/auth/logout',
+								{},
+								{
+										headers:{
+												'Authorization': this.TOKEN,
+										}
+								}
+						).then(()=> {     
+								this.$store.dispatch('auth/logout');	
+								this.$store.dispatch('uifront/reinit');	
+								this.$store.dispatch('uiadmin/reinit');	
+								this.$router.push('/');
+						})
+						.catch(() => {
+								this.$store.dispatch('auth/logout');	
+								this.$store.dispatch('uifront/reinit');	
+								this.$store.dispatch('uiadmin/reinit');	
+								this.$router.push('/');
+						});
+				},
+				isBentukPT (bentuk_pt)
+				{
+						return this.$store.getters['uifront/getBentukPT']==bentuk_pt?true:false;
+				}
 	},
-    computed:{
-        ...mapGetters('auth',{
-            AUTHENTICATED:'Authenticated',  
-            ACCESS_TOKEN:'AccessToken',          
-            TOKEN:'Token', 
-            DEFAULT_ROLE:'DefaultRole',         
-            ROLE:'Role',
-            CAN_ACCESS:'can',         
-            ATTRIBUTE_USER:'AttributeUser',               
-        }),
-        APP_NAME ()
-        {
-            return process.env.VUE_APP_NAME;
-        },
-        photoUser()
+		computed:{
+				...mapGetters('auth',{
+						AUTHENTICATED:'Authenticated',  
+						ACCESS_TOKEN:'AccessToken',          
+						TOKEN:'Token', 
+						DEFAULT_ROLE:'DefaultRole',         
+						ROLE:'Role',
+						CAN_ACCESS:'can',         
+						ATTRIBUTE_USER:'AttributeUser',               
+				}),
+				APP_NAME ()
+				{
+						return process.env.VUE_APP_NAME;
+				},
+				photoUser()
 		{
 			let img=this.ATTRIBUTE_USER('foto');
 			var photo;
@@ -137,27 +137,27 @@ export default {
 				photo = this.$api.storageURL+'/'+img;	
 			}
 			return photo;
-        },  
-    },
-    watch: {
-        loginTime:{
-            handler(value)
-            {
-                
-                if (value >= 0)
-                {
-                    setTimeout(() => { 
-                        this.loginTime=this.AUTHENTICATED==true?this.loginTime+1:-1;                                                                     
+				},  
+		},
+		watch: {
+				loginTime:{
+						handler(value)
+						{
+								
+								if (value >= 0)
+								{
+										setTimeout(() => { 
+												this.loginTime=this.AUTHENTICATED==true?this.loginTime+1:-1;                                                                     
 					}, 1000);
-                }
-                else
-                {
-                    this.$store.dispatch('auth/logout');
-                    this.$router.replace('/login');
-                }
-            },
-            immediate:true
-        },        
-    }
+								}
+								else
+								{
+										this.$store.dispatch('auth/logout');
+										this.$router.replace('/login');
+								}
+						},
+						immediate:true
+				},        
+		}
 }
 </script>
