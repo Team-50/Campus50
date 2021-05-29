1605f015bd064b018f3667c71860cd0444d8c63c
Kevin Perdana <45634322+kevinperdana@users.noreply.github.com>
2021-05-29 13:32:08 +0700

    Update ReportSKKelulusan.blade.php

--- backend/resources/views/spmb/ReportSKKelulusan.blade.php
+++ backend/resources/views/spmb/ReportSKKelulusan.blade.php
@@ -5,19 +5,26 @@ body {
   font-family:'Times New Roman';
   font-size:14px;
 }
-@page {  
-	footer: page-footer;
+table.t1 {
+  margin-left:30px;
+}
+table.t2 {
+  margin-left:30px;
 }
 img {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
+  display:block;
+  margin-top:5px;
+  margin-bottom:5px;
+  margin-left:auto;
+  margin-right:auto;
+}
+@page {  
+	footer: page-footer;
 }
 </style>
 </head>
 <body>
 @include('spmb.ReportHeaderPMB')
-<br>
 <br>
 <table>
   <tr>
@@ -36,50 +43,61 @@ img {
     <td><strong>Surat Keterangan Lulus Tes Calon Mahasiswa</strong></td>
   </tr>
 </table>
-<p>
-  Kepada Yth.<br>
-  <strong>Sdr./i {{strtoupper($formulir->nama_mhs)}}</strong><br>
-  DI – <br>
-  &nbsp;&nbsp;&nbsp;<u>Tempat</u>
+<p style="text-align: justify;text-justify: inter-word;text-indent:50px;">
+  Panitia Seleksi Penerimaan Mahasiswa Baru Sekolah Tinggi Teknologi Indonesia Tanjungpinang T.A. {{$formulir->ta}}/{{$formulir->ta+1}}, setelah memperhatikan hasil Ujian Sdr/i dalam Seleksi Penerimaan Mahasiswa Baru yang dilaksanakan pada tanggal {{$tanggal_lulus}} atas nama calon mahasiswa:
 </p>
+<table class="t1">
+  <tr>
+    <td width="100">Nama</td>
+    <td>:</td>
+    <td><b>{{$formulir->nama_mhs}}</b></td>
+  </tr>
+  <tr>
+    <td>No. Pendaftaran</td>
+    <td>:</td>
+    <td><b>{{$formulir->formulir}}</b></td>
+  </tr>
+</table>
 <p style="text-align: justify;text-justify: inter-word;">
-Puji syukur kita panjatkan kehadirat 
+Dengan ini Sdr/i dinyatakan  untuk pilihan jurusan, <b>JURUSAN</b>.<br>
+Dengan ketentuan bahwa pendaftaran sebagai Calon Mahasiswa Baru STT Indonesia T.A. {{$formulir->ta}}/{{$formulir->ta+1}}, Sdr/i diwajibkan melakukan <b>registrasi paling lambat</b>:
 </p>
-<p style="text-align: justify;text-justify: inter-word;">
-Menindak lanjuti hasil TES Kompetensi pada tanggal {{$tanggal_lulus}} maka dengan ini kami sampaikan bahwa saudara/i telah LULUS tes sebagai mahasiswa STT Indonesia Tanjungpinang tahun akademik {{$formulir->ta}}/{{$formulir->ta+1}}.
+<table class="t2">
+  <tr>
+    <td width="100">Hari/Tanggal</td>
+    <td>:</td>
+    <td><b>{{$next_day}}</b></td>
+  </tr>
+  <tr>
+    <td>Tempat</td>
+    <td>:</td>
+    <td><b>Kampus STT Indonesia Tanjungpinang, Jl. Pompa Air No.28 - Tanjungpinang</b></td>
+  </tr>
+  <tr>
+    <td>Jam</td>
+    <td>:</td>
+    <td><b>09:00 s.d. 16:00 WIB</b></td>
+  </tr>
+</table>
+
+<p style="text-align: justify;text-justify: inter-word;text-indent:50px;">
+  Pembayaran Registrasi dapat dilakukan langsung ke Bank BRI pada setiap jam kerja dan jika sampai dengan batas waktu tersebut diatas Sdr/i <b>tidak melakukan registrasi</b>, maka hak Sdr/i sebagai Calon Mahasiswa Baru Sekolah Tinggi Teknologi Indonesia Tanjungpinang T.A {{$formulir->ta}}/{{$formulir->ta+1}} di anggap mengundurkan diri. Dan apabila Sdr/i ingin melakukan pendaftaran, Sdr/I masih diberikan kesempatan untuk melakukan pendaftaran pada gelombang berikutnya.
 </p>
-<p style="text-align: justify;text-justify: inter-word;">
-Sebagai kelengkapan proses penerimaan mahasiswa baru, kami harapkan kehadiran Saudara/i untuk melakukan pendaftaran ulang pada kampus STMIK BANDUNG BALI segera setelah pelaksanaan TES Kompetensi Calon Mahasiswa selesai dilaksanakan paling lambat tanggal {{$next_day}} dengan melengkapi beberapa persyaratan sebagai berikut :
-</p>
-<ol>
-  <li>
-    Membayar Biaya dengan Rincian Biaya sebagai berikut :
-    <ul>
-      <li>Uang SPP</li>
-      
-      
-    </ul>  
-  </li>
-  <li>
-    Metode Pembayaran :
-    <ul>
-      <li>Transfer</li>      
-    </ul>  
-  </li>  
-</ol>
-<p>Demikian surat pemberitahuan ini, atas perhatian Saudara/i, kami sampaikan terima kasih.</p>
-<p>
+<p style="text-align: justify;text-justify: inter-word;text-indent:50px;">Demikian Surat Keputusan ini disampaikan untuk dapat dipergunakan sebagaimana mestinya, Terima kasih.</p>
+<br>
+<p style="text-align: right;">
   Tanjungpinang, {{$tanggal_lulus}}<br>
   Kepala Bagian Marketing,<br>
-  <img src="{{$sign_qrcode}}" height="150"><br>
-  <u>{{$signature->nama}}</u><br>  
+  <img src="{{$sign_qrcode}}" height="80"><br>
+  <u>{{$signature->nama}}</u><br>
   NIPY. {{$signature->nik}}
 </p>
 <htmlpagefooter name="page-footer">
   <hr>
-  Jl. Pompa Air No.28<br>
-  Email: info@sttindonesia.ac.id<br>
-  Web: https://www.sttindonesia.ac.id
+  <br>
+  Jl. Pompa Air No.28 <br/>
+  Email: info@sttindonesia.ac.id <br/>
+  Web: https://www.sttindonesia.ac.idd
 </htmlpagefooter>
 </body>
 </html>
