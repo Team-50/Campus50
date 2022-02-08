d78d54b5b998e10e2a609386d4610b08dc35a132
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2022-02-08 16:25:21 +0700

    perbaiki lazy src

--- frontend/src/views/pages/front/Home.vue
+++ frontend/src/views/pages/front/Home.vue
@@ -15,15 +15,15 @@
 										</v-carousel>-->
 										<v-img
 												class="hidden-sm-and-down"
-												lazy-src="$api.storageURL/storage/images/sliders/banner3.jpg"
+												:lazy-src="$api.storageURL + '/storage/images/sliders/banner3.jpg'"
 												max-height="auto"
 												max-width="100%"
-												:src="$api.url+'/storage/images/sliders/banner3.jpg'"
+												:src="$api.url + '/storage/images/sliders/banner3.jpg'"
 										></v-img>
 										<br>
 										<v-img
 												class="hidden-sm-and-down"
-												lazy-src="$api.storageURL/storage/images/sliders/banner5.jpg"
+												:lazy-src="$api.storageURL + '/storage/images/sliders/banner5.jpg'"
 												max-height="auto"
 												max-width="100%"
 												:src="$api.url+'/storage/images/sliders/banner5.jpg'"
@@ -33,14 +33,14 @@
 								<v-col cols="12" class="hidden-lg-and-up pa-0">
 										<v-img
 												class="hidden-lg-and-up mb-2"
-												lazy-src="$api.storageURL/storage/images/sliders/banner3_1.1.jpg"
+												:lazy-src="$api.storageURL + 'storage/images/sliders/banner3.jpg'"
 												max-height="auto"
 												max-width="100%"
-												:src="$api.url+'/storage/images/sliders/banner3_1.1.jpg'"
+												:src="$api.url+'/storage/images/sliders/banner3.jpg'"
 										></v-img>
 										<v-img
 												class="hidden-lg-and-up"
-												lazy-src="$api.storageURL/storage/images/sliders/banner5.jpg"
+												:lazy-src="$api.storageURL + '/storage/images/sliders/banner5.jpg'"
 												max-height="auto"
 												max-width="100%"
 												:src="$api.url+'/storage/images/sliders/banner5.jpg'"
